0deb86669e099f2d321dee30bbb17a6ed53b2a51
Alexandre Briand <alexandre.briand@polymtl.ca>
2017-02-20 14:01:14 -0500

    graphs loglog

--- traceGraphs.sh
+++ traceGraphs.sh
@@ -3,5 +3,5 @@
 for data in $(ls results/means/)
 do
 	sed -i 's/,/ /g' results/means/$data	
-	gnuplot -e "set xrange [0:600000]; set yrange[0:0.0001]; set term png; set output \"graphs/${data%.csv}.png\"; plot \"results/means/$data\" with linespoints"
+	gnuplot -e "set xrange [500:600000]; set yrange[0.00001:0.0001]; set logscale xy; set term png; set output \"graphs/${data%.csv}_loglog.png\"; plot \"results/means/$data\" with linespoints"
 done
