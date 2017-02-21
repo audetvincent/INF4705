0bdfb57c651a194390526756d775b2566d9c5fc2
Alexandre Briand <alexandre.briand@polymtl.ca>
2017-02-21 18:02:49 -0500

    debut du TP2 : lecture du data et structure des scripts

---
+++ TP2/src/DAG.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+
+# From : https://github.com/thieman/py-dag/blob/master/dag/__init__.py
+
+from copy import copy, deepcopy
+from collections import deque
+
+try:
+    from collections import OrderedDict
+except:
+    from ordereddict import OrderedDict
+
+
+class DAGValidationError(Exception):
+    pass
+
+
+class DAG(object):
+    """ Directed acyclic graph implementation. """
+
+    def __init__(self):
+        """ Construct a new DAG with no nodes or edges. """
+        self.reset_graph()
+
+    def add_node(self, node_name, graph=None):
+        """ Add a node if it does not exist yet, or error out. """
+        if not graph:
+            graph = self.graph
+        if node_name in graph:
+            raise KeyError('node %s already exists' % node_name)
+        graph[node_name] = set()
+
+    def add_node_if_not_exists(self, node_name, graph=None):
+        try:
+            self.add_node(node_name, graph=graph)
+        except KeyError:
+            pass
+
+    def delete_node(self, node_name, graph=None):
+        """ Deletes this node and all edges referencing it. """
+        if not graph:
+            graph = self.graph
+        if node_name not in graph:
+            raise KeyError('node %s does not exist' % node_name)
+        graph.pop(node_name)
+
+        for node, edges in graph.iteritems():
+            if node_name in edges:
+                edges.remove(node_name)
+
+    def delete_node_if_exists(self, node_name, graph=None):
+        try:
+            self.delete_node(node_name, graph=graph)
+        except KeyError:
+            pass
+
+    def add_edge(self, ind_node, dep_node, graph=None):
+        """ Add an edge (dependency) between the specified nodes. """
+        if not graph:
+            graph = self.graph
+        if ind_node not in graph or dep_node not in graph:
+            raise KeyError('one or more nodes do not exist in graph')
+        test_graph = deepcopy(graph)
+        test_graph[ind_node].add(dep_node)
+        is_valid, message = self.validate(test_graph)
+        if is_valid:
+            graph[ind_node].add(dep_node)
+        else:
+            raise DAGValidationError()
+
+    def delete_edge(self, ind_node, dep_node, graph=None):
+        """ Delete an edge from the graph. """
+        if not graph:
+            graph = self.graph
+        if dep_node not in graph.get(ind_node, []):
+            raise KeyError('this edge does not exist in graph')
+        graph[ind_node].remove(dep_node)
+
+    def predecessors(self, node, graph=None):
+        """ Returns a list of all predecessors of the given node """
+        if graph is None:
+            graph = self.graph
+        return [key for key in graph if node in graph[key]]
+
+    def downstream(self, node, graph=None):
+        """ Returns a list of all nodes this node has edges towards. """
+        if graph is None:
+            graph = self.graph
+        if node not in graph:
+            raise KeyError('node %s is not in graph' % node)
+        return list(graph[node])
+    
+    def all_leaves(self, graph=None):
+        """ Return a list of all leaves (nodes with no downstreams) """
+        if graph is None:
+            graph = self.graph
+        return [key for key in graph if not graph[key]]
+
+    def reset_graph(self):
+        """ Restore the graph to an empty state. """
+        self.graph = OrderedDict()
+
+    def ind_nodes(self, graph=None):
+        """ Returns a list of all nodes in the graph with no dependencies. """
+        if graph is None:
+            graph = self.graph
+
+        dependent_nodes = set(node for dependents in graph.itervalues() for node in dependents)
+	return [node for node in graph.keys() if node not in dependent_nodes]
+
+    def validate(self, graph=None):
+        """ Returns (Boolean, message) of whether DAG is valid. """
+        graph = graph if graph is not None else self.graph
+        if len(self.ind_nodes(graph)) == 0:
+            return (False, 'no independent nodes detected')
+        try:
+            self.topological_sort(graph)
+        except ValueError:
+            return (False, 'failed topological sort')
+	return (True, 'valid')
+
+    def topological_sort(self, graph=None):
+        """ Returns a topological ordering of the DAG.
+        Raises an error if this is not possible (graph is not valid).
+        """
+        if graph is None:
+            graph = self.graph
+
+        in_degree = {}
+        for u in graph:
+            in_degree[u] = 0
+
+        for u in graph:
+            for v in graph[u]:
+                in_degree[v] += 1
+
+        queue = deque()
+        for u in in_degree:
+            if in_degree[u] == 0:
+                queue.appendleft(u)
+
+        l = []
+        while queue:
+            u = queue.pop()
+            l.append(u)
+            for v in graph[u]:
+                in_degree[v] -= 1
+                if in_degree[v] == 0:
+                    queue.appendleft(v)
+
+        if len(l) == len(graph):
+            return l
+        else:
+	    raise ValueError('graph is not acyclic')
+
+    def size(self):
+        return len(self.graph)
---
+++ TP2/src/vorace.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# Algorithme vorace permettant de calculer le nombre d'extensions lineaires dans un graphe orienté sans cycle
+
+import sys
+import datetime
+from DAG import DAG
+
+# Ouverture du fichier de donnees
+file = open(str(sys.argv[1]), "r")
+
+# Creation du graphe
+info = file.readline().split()
+nbVertices = int(info[0])
+nbArcs = int(info[1])
+dag = DAG()
+for i in range(nbVertices):
+    dag.add_node(i)
+for i in range(nbArcs):
+    arc = file.readline().split()
+    dag.add_edge(int(arc[0]), int(arc[1]))
+
+# Denombrement des solutions
+nbOrderings = 0
+time_start = datetime.datetime.now()
+# TODO here
+time_end = datetime.datetime.now()
+
+# Verification des parametres fournis
+if "-p" in sys.argv or "--print" in sys.argv:
+    print nbOrderings
+
+if "-t" in sys.argv or "--time" in sys.argv:
+    print (time_end - time_start).total_seconds()
